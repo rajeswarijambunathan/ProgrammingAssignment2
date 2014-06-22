a18e0b479678aab8c918dadace1d279b86d41fa3
rajeswarijambunathan <rajeswarijambunathan@gmail.com>
2014-06-22 20:28:48 +0530

    Assignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## This function creates a cache of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+  m<-NULL
+  set<-function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get<-function() x
+  setinv<-function(solve) m<<- solve
+  getinv<-function() m
+  list(set=set, get=get,
+       setinv=setinv,
+       getinv=getinv)
 
 }
 
 
-## Write a short comment describing this function
+## This functions returns the inverse of the cached matrix if the matrix is not changed. 
+## ginv from MASS package can be used as a replacement to solve fn
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x=matrix(), ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m<-x$getinv()
+  if(!is.null(m)){
+    message("getting cached data if the matrix is not changes")
+    return(m)
+  }
+  matrix<-x$get()
+  m<-solve(matrix, ...)
+  x$setinv(m)
+  m
+  
 }
+## To Run:
+##b<-makeCacheMatrix()
+##b$set(matrix(c(0, 2, 1, 5,3,4,4,2,1), nrow = 3, ncol = 3, byrow = TRUE))
+##cacheSolve(b)
